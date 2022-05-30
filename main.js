98e433184a6e20b3f5eac6e3534ff1a8acc6a92e
Aizat <s.aizat21@gmail.com>
2022-05-30 14:36:24 +0600

    updated links

--- main.js
+++ main.js
@@ -1,51 +1,51 @@
 const cardArray = [
     {
         name: 'fries',
-        img:'/images/fries.png'
+        img:'./images/fries.png'
     },
     {
         name: 'cheeseburger',
-        img:'/images/cheeseburger.png'
+        img:'./images/cheeseburger.png'
     },
     {
         name: 'hotdog',
-        img:'/images/hotdog.png'
+        img:'./images/hotdog.png'
     },
     {
         name: 'ice-cream',
-        img:'/images/ice-cream.png'
+        img:'./images/ice-cream.png'
     },
     {
         name: 'milkshake',
-        img:'/images/milkshake.png'
+        img:'./images/milkshake.png'
     },
     {
         name: 'pizza',
-        img:'/images/pizza.png'
+        img:'./images/pizza.png'
     },
     {
         name: 'fries',
-        img:'/images/fries.png'
+        img:'./images/fries.png'
     },
     {
         name: 'cheeseburger',
-        img:'/images/cheeseburger.png'
+        img:'./images/cheeseburger.png'
     },
     {
         name: 'hotdog',
-        img:'/images/hotdog.png'
+        img:'./images/hotdog.png'
     },
     {
         name: 'ice-cream',
-        img:'/images/ice-cream.png'
+        img:'./images/ice-cream.png'
     },
     {
         name: 'milkshake',
-        img:'/images/milkshake.png'
+        img:'./images/milkshake.png'
     },
     {
         name: 'pizza',
-        img:'/images/pizza.png'
+        img:'./images/pizza.png'
     }
 ]
 
@@ -60,7 +60,7 @@ const cardsWon = []
 function createBoard() {
     for(let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
-        card.setAttribute('src', '/images/blank.png')
+        card.setAttribute('src', './images/blank.png')
         card.setAttribute('data-id', i)
         card.addEventListener('click', flipCard)
         gridDisplay.appendChild(card)
@@ -73,21 +73,21 @@ function checkMatch () {
     const optionOneId = cardsChosenIds[0]
     const optionTwoId = cardsChosenIds[1]
     if (optionOneId == optionTwoId) {
-        cards[optionOneId].setAttribute('src', '/images/blank.png')
-        cards[optionTwoId].setAttribute('src', '/images/blank.png')
+        cards[optionOneId].setAttribute('src', './images/blank.png')
+        cards[optionTwoId].setAttribute('src', './images/blank.png')
         
     }
 
     if (cardsChosen[0] == cardsChosen[1]){
         
-        cards[optionOneId].setAttribute('src', '/images/white.png')
-        cards[optionTwoId].setAttribute('src', '/images/white.png')
+        cards[optionOneId].setAttribute('src', './images/white.png')
+        cards[optionTwoId].setAttribute('src', './images/white.png')
         cards[optionOneId].removeEventListener('click', flipCard)
         cards[optionTwoId].removeEventListener('click', flipCard) 
         cardsWon.push(cardsChosen)
     } else {
-        cards[optionOneId].setAttribute('src', '/images/blank.png')
-        cards[optionTwoId].setAttribute('src', '/images/blank.png')
+        cards[optionOneId].setAttribute('src', './images/blank.png')
+        cards[optionTwoId].setAttribute('src', './images/blank.png')
         
     }
     resultDisplay.textContent = cardsWon.length
